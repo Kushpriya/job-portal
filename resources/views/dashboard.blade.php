e826a7d85042185d8771f4006063c1465fd120df
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-04-16 19:54:00 +0545

    job portal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    <style>  
+        a{  
+            text-decoration: none;  
+            color: green;  
+            font-size: 20px;  
+            padding: 8px;  
+        }  
+        a:hover{  
+            color: blue;  
+        } 
+      
+    </style>  
 </head>
 <body>
-    <h1>gfyrcgvuhbjkn</h1>
+    <nav>  
+        <a href="home">Home</a>   
+        <a href="about">About us</a>   
+        <a href="jobs">All Jobs</a>   
+        <a href="contact">Contact-us</a>
+        <a href="login">Account</a>
+
+     </nav>  
+     
     
 </body>
 </html>
